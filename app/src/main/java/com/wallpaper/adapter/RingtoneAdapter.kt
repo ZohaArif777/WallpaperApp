742a8b072ddfacb655742ad55ae7fa7050b74d24
Zoha <zohaarif4567@gmail.com>
2025-03-08 15:00:27 +0500

    added ringtone

---
+++ app/src/main/java/com/wallpaper/adapter/RingtoneAdapter.kt
@@ -0,0 +1,57 @@
+package com.wallpaper.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.wallpaper.R
+import com.wallpaper.features.RingtonePlayer
+import com.wallpaper.dataclass.Ringtones
+import com.wallpaper.databinding.RingtoneListBinding
+
+class RingtoneAdapter(
+    private val context: Context,
+    private val ringtoneList: List<Ringtones>,
+    private val soundType: String
+) : RecyclerView.Adapter<RingtoneAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: RingtoneListBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            RingtoneListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ringtone = ringtoneList[position]
+        with(holder.binding) {
+            text.text = ringtone.name
+            size.text = ringtone.size
+            duration.text = ringtone.duration
+            arrowButton.setOnClickListener {
+                val intent = Intent(context, RingtonePlayer::class.java)
+                context.startActivity(intent)
+            }
+            val typeface = ResourcesCompat.getFont(context, R.font.outfit)
+            text.typeface = typeface
+            size.typeface = typeface
+            duration.typeface = typeface
+
+            root.setOnClickListener {
+                val intent = Intent(context, RingtonePlayer::class.java).apply {
+                    putExtra("RINGTONE_NAME", ringtone.name)
+                    putExtra("RINGTONE_SIZE", ringtone.size)
+                    putExtra("RINGTONE_DURATION", ringtone.duration)
+                    putExtra("RINGTONE_RES_ID", ringtone.rawResId)
+                    putExtra("SOUND_TYPE", soundType)
+                }
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = ringtoneList.size
+}
---
+++ app/src/main/java/com/wallpaper/adapter/RingtoneViewpagerAdapter.kt
@@ -0,0 +1,13 @@
+package com.wallpaper.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class RingtoneViewpagerAdapter(
+    private val fragment: List<Fragment>, fragmentManager: FragmentManager, lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int = fragment.size
+    override fun createFragment(position: Int): Fragment = fragment[position]
+}
